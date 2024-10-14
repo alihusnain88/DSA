683d1868cc66a595f7c02102333d3574824b6090
Ali Husnain <muhammadalihusnain100@gmail.com>
2024-10-15 00:19:22 +0500

    Added Largest Rectangle In Histogram.cpp

---
+++ Leetcode/Monotonic Stack/Largest Rectangle in Histogram.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int size = heights.size();
+        vector<int> nextSmallers (size, size);
+        vector<int> prevSmallers (size, -1);
+        stack <int> s;
+        for(int i=0; i<size; i++)
+        {
+            while(!s.empty() && heights[s.top()] > heights[i])
+            {
+                nextSmallers[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty())
+        {
+            s.pop();
+        }
+        for(int i=size-1; i>=0; i--)
+        {
+            while(!s.empty() && heights[s.top()] > heights[i])
+            {
+                prevSmallers[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+
+        int maxArea = 0;
+        for(int i=0; i<size; i++)
+        {
+            int currArea = (nextSmallers[i] - prevSmallers[i] - 1) * heights[i];
+            maxArea = max(currArea, maxArea);
+        }
+        return maxArea;
+
+    }
+};
+
+
+
+
+
+

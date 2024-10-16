13f650f99d38b07d940814a85dbfe940229ea233
Ali Husnain <muhammadalihusnain100@gmail.com>
2024-10-16 22:06:54 +0500

    Added 'Daily Temperatures.cpp'

---
+++ Leetcode/Monotonic Stack/Daily Temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int size = temperatures.size();
+        vector <int> nextWarmerTemps(size, 0);
+        stack <int> monotonic;
+        for(int curr = 0; curr < size; curr++)
+        {
+            while(!monotonic.empty() && temperatures[monotonic.top()] < temperatures[curr])
+            {
+                nextWarmerTemps[monotonic.top()] = curr - monotonic.top();
+                monotonic.pop();
+            }
+            monotonic.push(curr);
+        }
+        return nextWarmerTemps;
+    }
+};
